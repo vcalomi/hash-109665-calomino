f63521d04fa81a39d2dbe3d47a3d36d65f47d7c9
vcalomi <vcalomino@fi.uba.ar>
2023-05-31 16:21:44 -0300

    pruebas_creacion

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,26 @@
 #include "pa2m.h"
+#include "src/hash.h"
 
-void prueba_simple()
+void pruebas_creacion()
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+	size_t capacidad_1 = 4;
+	hash_t *hash_1 = hash_crear(capacidad_1);
+	pa2m_afirmar(hash_1 != NULL,
+		     "Se puede rear un hash con capacidad mayor a 3");
+	capacidad_1 = 2;
+	hash_t *hash_2 = hash_crear(capacidad_1);
+	pa2m_afirmar(hash_2 != NULL,
+		     "Se puede rear un hash con capacidad menor a 3");
+	hash_destruir(hash_1);
+	pa2m_afirmar(
+		hash_1 == NULL,
+		"La funcion destructora libera la memoria del hash correctamente");
+	hash_destruir(hash_2);
 }
-
 int main()
 {
-	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
+	pa2m_nuevo_grupo("\nPruebas de Creacion");
+	pruebas_creacion();
 
 	return pa2m_mostrar_reporte();
 }
